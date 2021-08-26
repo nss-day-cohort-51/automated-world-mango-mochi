dc1b4b0d85648a3d4d3988b9fba9f59f7bdb426e
ldabudiab <ldabudiab@gmail.com>
2021-08-26 09:45:11 -0500

    made edits to nav bar and iv page

--- JS/ivorycoastJS/ivformat.js
+++ JS/ivorycoastJS/ivformat.js
@@ -5,7 +5,7 @@ export const ivyCity = (ivyCityObj) => {
     <ul>
         <li class="city-details">Poulation: ${ivyCityObj.population} </li>
         <li class="city-details">Fact: ${ivyCityObj.fact}</li>
-        <li class="city-details">Learn More: ${ivyCityObj.link}</li>
+        <li class="city-details"><a href= ${ivyCityObj.link}> Learn More</a>
     </ul>
     </article>
     
@@ -17,9 +17,9 @@ export const ivyLndMrk = (ivLndMrkObj) => {
     <article>
     <h3 class="city-name">${ivLndMrkObj.name}</h3>
     <ul>
-        <li class="landmark-details">Built: ${ivLndMrkObj.built} inches</li>
+        <li class="landmark-details">Built: ${ivLndMrkObj.built} </li>
         <li class="landmark-details">Fact: ${ivLndMrkObj.fact}</li>
-        <li class="landmark-details">Learn More: ${ivLndMrkObj.link}</li>
+        <li class="landmark-details"><a href= ${ivLndMrkObj.link}> Learn More</a>
     </ul>
     </article>
    
@@ -31,9 +31,9 @@ export const ivyPeople = (ivPeopleObj) => {
     <article>
     <h3 class="name">${ivPeopleObj.people}</h3>
     <ul>
-        <li class="people-details">Occupation: ${ivPeopleObj.occupation} inches</li>
+        <li class="people-details">Occupation: ${ivPeopleObj.occupation} </li>
         <li class="people-details">Fact: ${ivPeopleObj.fact}</li>
-        <li class="people-details">Learn More: ${ivPeopleObj.link}</li>
+        <li class="people-details"><a href= ${ivPeopleObj.link}> Learn More</a> </li>
     </ul>
     </article>
     
